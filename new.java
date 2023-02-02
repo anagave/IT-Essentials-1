30c1c6089aeeaf040d7fb75cd70651a7d273afb9
tejswii <gtejaswi0407@gmail.com>
2023-02-01 19:48:50 -0700

    trytg

---
+++ new.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Newest {
+
+int s;
+public int sum(int a, int b){
+	s = a+b;
+	System.out.println(s);
+	return s;
+}
+
+public static void main(String args[]){
+	Newest p = new Newest();
+	p.sum(2,3);
+}
+}
